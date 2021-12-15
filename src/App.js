5313943d5408fe635efe55ece2cc4d46ce43e3b0
Mocroski Lucas <devmocroski@gmail.com>
2021-12-15 13:59:28 -0300

    First commit.Almost finish the first part

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import react, {useEffect, useState} from 'react';
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+
+const spotify = new SpotifyWebApi();
+
+function App() {
+const [token, setToken] = useState(null);
+
+  useEffect(() =>{
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log(':D', user);
+      })
+    }
+
+    console.log("I HAVE A TOKENNNNN", token);
+  }, []);
+
+  return <div className="App">{token ?
+      <Player /> :
+      <Login />}</div>
+}
+
+export default App;
---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Player() {
+    return(
+        <div>
+            <h1> WELCOME TO SPOTIFY</h1>
+        </div>
+    );
+}
+
+export default Player;
